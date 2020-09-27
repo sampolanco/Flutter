4dc96f4bf89a38bdbc653b6c916b888f5eb08d02
sampolanco <samy_ckl@hotmail.com>
2020-09-27 15:19:21 -0500

    Se actualiza gitignore

--- b_componentes/lib/main.dart
+++ b_componentes/lib/main.dart
@@ -7,7 +7,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Componentes',
+      title: 'Componentes de flutter',
       debugShowCheckedModeBanner: false,
       home: Scaffold(
         appBar: AppBar(
